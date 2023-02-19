0e04001a6950ac1b45985a7e393ba084c71bc54f
Navjyotsihdu34 <89522797+Navjyot34@users.noreply.github.com>
2023-02-19 14:55:33 +0530

    Sets- union, intersection, diffrence

---
+++ assignment2.py
@@ -0,0 +1,14 @@
+# Python Program to Illustrate Different Set Operations Here we have to define two set variables and we have to perform different set operations: union, intersection, difference and symmetric difference.
+
+  
+
+A = {0, 2, 4, 6, 8};
+B = {1, 2, 3, 4, 5};
+  
+print("Union of A and B :", A | B)
+  
+print("Intersection of A and B :",  A & B)
+  
+print("Difference of A and B :", A - B)
+  
+print("Symmetric difference of A and B :", A ^ B)
